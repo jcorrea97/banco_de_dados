88b4b06a630c26394cee726bbdb03bfb28c3d2ed
Jose Roberto Correa <jroberto.cr1@gmail.com>
2019-11-20 21:33:35 -0300

    metodo de inserir, selecionar por id, e selecionar tudo

--- src/executaveis/TesteInsere.java
+++ src/executaveis/TesteInsere.java
@@ -2,17 +2,21 @@ package executaveis;
 
 import java.util.Calendar;
 
-import dao.ContatoDAO;
-import dao.PublicacaoDAO;
-import model.Contato;
+import dao.*;
+import model.*;
 
 public class TesteInsere {
 
 	public static void main(String[] args) {
-		PublicacaoDAO pub = new PublicacaoDAO();
+//		PublicacaoDAO pub = new PublicacaoDAO();
+		EditoraDAO edit = new EditoraDAO();
+//		edit.adiciona("Editora Ricardg");
 //		pub.adiciona("Livro", "Guaianases");
-		System.out.println(pub.remove(3));
+//		System.out.println(pub.remove(3));
 //		System.out.println(pub.selecionaTudo());
+	System.out.println(edit.seleciona(2));
+//	System.out.println(edit.selecionaTudo());
+//		System.out.println(edit.remove(1));
 	}
 
 }
--- src/model/Editora.java
+++ src/model/Editora.java
@@ -3,6 +3,11 @@ package model;
 public class Editora {
 	private Integer id_editora;
 	private String editora;
+	
+	public Editora(String editora){
+		setEditora(editora);
+	}
+	
 	public Integer getId_editora() {
 		return id_editora;
 	}
@@ -14,6 +19,14 @@ public class Editora {
 	}
 	public void setEditora(String editora) {
 		this.editora = editora;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format(
+				"{ id_editora: %d, editora: %s  }",
+				getId_editora(),
+				getEditora());
 	}
 	
 	
